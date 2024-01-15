e53e3efaf6e9ae015197aac533044addb398295c
Vasster <vasilhspinhs@gmail.com>
2024-01-15 20:52:43 +0200

    Population Manager,Playtime Manager
    Added Population Manager.
    Added PlayTime Manager.

---
+++ Village Defenders/Assets/Scripts/PlaytimeManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlaytimeManager : MonoBehaviour
+{
+    public static PlaytimeManager instance;
+    private float currentplayTime;
+    [SerializeField] private TextMeshProUGUI playtimeText;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        currentplayTime += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(currentplayTime / 60);
+        int seconds = Mathf.FloorToInt(currentplayTime % 60);
+        string currentTime = string.Format("{0:00}:{1:00}", minutes, seconds);
+        playtimeText.text = "Current Playtime: " + currentTime;
+    }
+
+
+}
