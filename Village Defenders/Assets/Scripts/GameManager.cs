06d957f43868ef2c5f21c76f0d88a61a1fd482f9
Vasster <vasilhspinhs@gmail.com>
2024-01-15 19:23:40 +0200

    New Changes
    -Added Game Manager that handles the shop system by instatiating the veggies panels, the defences and npcs panels to the shop.
    -Added Veggie panel that shows the vegetable name,price,image and you can buy it.

---
+++ Village Defenders/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    [Header("Shop")]
+    public List<ScriptableVegetables> veggiesScriptable;
+    public List<ScriptableNPCS> NpcsScriptable;
+    public List<ScriptableDefences> defencesScriptable;
+    public int coins = 100;
+    [SerializeField] private GameObject[] VeggiePanels;
+    [SerializeField] private GameObject[] NPCPanels;
+    [SerializeField] private GameObject[] defencesPanels;
+    public GameObject generalshopPanel;
+    [SerializeField] private GameObject veggiesShopPanel;
+    [SerializeField] private GameObject NpcsShopPanel;
+    [SerializeField] private GameObject defencesShopPanel;
+    [SerializeField] private TextMeshProUGUI coinsText;
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void OnEnable()
+    {
+        foreach (GameObject panelPrefab in VeggiePanels)
+        {
+            GameObject veggiesGameobjectPanel = Instantiate(panelPrefab, veggiesShopPanel.transform);
+            veggiesGameobjectPanel.transform.localPosition = Vector3.zero;
+        }
+        foreach (GameObject NpcPrefabPanel in NPCPanels)
+        {
+            GameObject NpcsGameobjectPanel = Instantiate(NpcPrefabPanel, NpcsShopPanel.transform);
+            NpcsGameobjectPanel.transform.localPosition = Vector3.zero;
+        }
+        foreach (GameObject defencesPrefabPanel in defencesPanels)
+        {
+            GameObject defencesGameobjectPanel = Instantiate(defencesPrefabPanel,defencesShopPanel.transform);
+            defencesGameobjectPanel.transform.localPosition = Vector3.zero;
+        }
+    }
+
+    private void Start()
+    {
+        generalshopPanel.SetActive(false);
+        veggiesShopPanel.SetActive(false);
+        NpcsShopPanel.SetActive(false);
+        defencesShopPanel.SetActive(false);
+
+        coinsText.text = "Coins: " + coins.ToString();
+    }
+}
---
+++ Village Defenders/Assets/Scripts/ItemPanels/VeggiePanel.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VeggiePanel : MonoBehaviour
+{
+    public ScriptableVegetables scriptableVegs;
+    public TextMeshProUGUI veggieName;
+    public Image veggieImage;
+    public TextMeshProUGUI veggiePriceText;
+    [SerializeField] Button button;
+    private void Start()
+    {
+        button = GetComponentInChildren<Button>();
+        veggieName.text = scriptableVegs.name;
+        veggieImage.sprite = scriptableVegs.spriteImage;
+        veggiePriceText.text = scriptableVegs.veggiePrice.ToString();
+        button.onClick.AddListener(BuyItem);
+
+    }
+    public void BuyItem()
+    {
+
+        if (GameManager.instance.coins >= scriptableVegs.veggiePrice)
+        {
+           GameManager.instance.coins -= scriptableVegs.veggiePrice;
+            FarmingManager.instance.PlantVegetable(scriptableVegs.veggieID);
+            GameManager.instance.generalshopPanel.SetActive(false);
+            GameManager.instance.veggiesScriptable.Add(scriptableVegs);
+            Debug.Log("Bought item" + scriptableVegs.name);
+         
+        }
+    }
+
+}
+
