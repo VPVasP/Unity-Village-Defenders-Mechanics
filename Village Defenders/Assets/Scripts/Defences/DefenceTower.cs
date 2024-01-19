7860076748f0b696613894e347ac7d5ed70775ce
Vasster <vasilhspinhs@gmail.com>
2024-01-19 12:08:54 +0200

    UI Fixes,Improvements on the tower defence code

--- Village Defenders/Assets/Scripts/Defences/DefenceTower.cs
+++ Village Defenders/Assets/Scripts/Defences/DefenceTower.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class DefenceTower : MonoBehaviour
@@ -8,61 +7,58 @@ public class DefenceTower : MonoBehaviour
     [SerializeField] private Transform firePosition;
     [SerializeField] private Transform canon;
     [SerializeField] private GameObject bullet;
-    [SerializeField] private Transform[] targets;
+    private List<Transform> targets = new List<Transform>();
+
     private void Start()
     {
-        GameObject[] targetObjects = GameObject.FindGameObjectsWithTag("Enemy");
-        targets = new Transform[targetObjects.Length];
-
-        for (int i = 0; i < targetObjects.Length; i++)
-        {
-            targets[i] = targetObjects[i].transform;
-        }
-        StartCoroutine(ShootTargetsDelay());
+        StartCoroutine(ShootTargetsRoutine());
     }
-    private void Update()
-    {
-        Transform closestTarget = GetClosestTarget(targets);
 
-        if (closestTarget != null)
-        {
-            RotateCanonTowardsTarget(closestTarget.position);
-        }
-    }
-    private IEnumerator ShootTargetsDelay()
+    private IEnumerator ShootTargetsRoutine()
     {
         while (true)
         {
+            FindTargets();
+
             Transform closestTarget = GetClosestTarget(targets);
-          
-       
 
             if (closestTarget != null)
             {
+                RotateCanonTowardsTarget(closestTarget.position);
                 ShootTargets(closestTarget.position);
             }
+
             yield return new WaitForSeconds(2f);
         }
     }
-    
+
+    private void FindTargets()
+    {
+        GameObject[] targetObjects = GameObject.FindGameObjectsWithTag("Enemy");
+        targets.Clear();
+
+        for (int i = 0; i < targetObjects.Length; i++)
+        {
+            targets.Add(targetObjects[i].transform); 
+        }
+    }
 
     private void ShootTargets(Vector3 targetPosition)
     {
         GameObject bulletClone = Instantiate(bullet, firePosition.position, Quaternion.identity);
-        bulletClone.GetComponent<Rigidbody>().AddForce(transform.forward *50);
-        Destroy(bulletClone,5f);
+        bulletClone.GetComponent<Rigidbody>().AddForce(transform.forward * 50);
+        Destroy(bulletClone, 5f);
     }
+
     private void RotateCanonTowardsTarget(Vector3 targetPosition)
     {
         Vector3 targetDirection = targetPosition - canon.position;
-
         float targetRotationZ = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
-
-        targetRotationZ = Mathf.Clamp(targetRotationZ, -40,40);
-
+        targetRotationZ = Mathf.Clamp(targetRotationZ, -40, 40);
         canon.rotation = Quaternion.Euler(0f, 0f, targetRotationZ);
     }
-    private Transform GetClosestTarget(Transform[] objects)
+
+    private Transform GetClosestTarget(List<Transform> objects)
     {
         Transform bestTarget = null;
         float closestDistance = float.MaxValue;
