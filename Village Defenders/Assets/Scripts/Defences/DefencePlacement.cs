dbdaf6dd2d473f72621484e6d025383d7c05ae22
Vasster <vasilhspinhs@gmail.com>
2024-01-20 10:31:13 +0200

    Defences UI Manager
    -Added defences ui manager that handles the numbers and text ui.
    -If an item has been placed in the defence placement script the defences ui enables and disables the correct UI.

--- Village Defenders/Assets/Scripts/Defences/DefencePlacement.cs
+++ Village Defenders/Assets/Scripts/Defences/DefencePlacement.cs
@@ -6,16 +6,42 @@ public class DefencePlacement : MonoBehaviour
     public bool hasBeenPlaced;
     public GameObject objectToInstantiate;
     public string[] keysToSpawn;
-    public TextMeshProUGUI pressKeyText;
     [SerializeField] private bool hasPressedKeyTwice;
     [SerializeField] private int keyNumberPresses;
+    [SerializeField] private DefencePlacementUI[] defencePlacementUI;
+
+
+    private void OnEnable()
+    {
+        defencePlacementUI = FindObjectsOfType<DefencePlacementUI>();
+        foreach (DefencePlacementUI defencePlacementUI in defencePlacementUI)
+        {
+
+            defencePlacementUI.NumberText.gameObject.SetActive(true);
+        }
+
+    }
+    private void OnDisable()
+    {
+
+        defencePlacementUI = FindObjectsOfType<DefencePlacementUI>();
+        foreach (DefencePlacementUI defencePlacementUI in defencePlacementUI)
+        {
+
+            defencePlacementUI.NumberText.gameObject.SetActive(false);
+        }
+
+    }
     private void Start()
     {
         hasBeenPlaced = false;
         this.enabled = false;
         keyNumberPresses = 0;
+        keyNumberPresses += 1;
+       
+
     }
-   
+
 
     private void Update()
     {
@@ -28,9 +54,16 @@ public class DefencePlacement : MonoBehaviour
                     Instantiate(objectToInstantiate, transform.position, Quaternion.identity);
                     hasBeenPlaced = true;
                     this.enabled = false;
-                    pressKeyText.gameObject.SetActive(false);
                     hasPressedKeyTwice = false;
                     keyNumberPresses += 1;
+                    this.enabled = false;
+                    foreach (DefencePlacementUI defencePlacementUI in defencePlacementUI)
+                    {
+
+                        defencePlacementUI.pressKeyText.gameObject.SetActive(false);
+                    }
+
+
                     break;
                 }
             }
@@ -44,12 +77,22 @@ public class DefencePlacement : MonoBehaviour
                 {
                     hasPressedKeyTwice = true;
                     keyNumberPresses += 1;
-                    pressKeyText.gameObject.SetActive(true);
-                    pressKeyText.text = "The position with the number " + key + " is already occupied";
+                    this.enabled = false;
+                    foreach (DefencePlacementUI defencePlacementUI in defencePlacementUI)
+                    {
+
+                        defencePlacementUI.pressKeyText.gameObject.SetActive(false);
+                    }
+
+
+
                     break;
                 }
             }
      
         }
+      
+
+        
     }
 }
---
+++ Village Defenders/Assets/Scripts/Defences/DefencePlacementUI.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DefencePlacementUI : MonoBehaviour
+{
+    [SerializeField] private DefencePlacement defencePlacement;
+    public TextMeshProUGUI NumberText;
+    private Canvas canvas;
+    private Camera mainCam;
+    public TextMeshProUGUI pressKeyText;
+    private void Start()
+    {
+        NumberText = GetComponentInChildren<TextMeshProUGUI>();
+        NumberText.transform.localScale = new Vector3(5, 5, 1);
+        NumberText.transform.localRotation = Quaternion.Euler(0f, -180f, 0f);
+        NumberText.gameObject.SetActive(true);
+        canvas = GetComponentInChildren<Canvas>();
+
+        defencePlacement = GetComponent<DefencePlacement>();
+
+        if (defencePlacement != null)
+        {
+            NumberText.text = "Press " + defencePlacement.keysToSpawn[0];
+        }
+        mainCam = Camera.main;
+        canvas.worldCamera = mainCam;
+        canvas.transform.localRotation = Quaternion.Euler(0f,0f, 0f);
+    }
+    public void UpdateUiIfNotPlaced()
+    {
+        NumberText.text = "";
+        pressKeyText.gameObject.SetActive(false);
+        NumberText.gameObject.SetActive(false);
+    }
+    public void UpdateUiIfPlaced()
+    {
+        NumberText.gameObject.SetActive(false);
+        pressKeyText.gameObject.SetActive(true);
+        pressKeyText.text = "The position with the number " + defencePlacement.keysToSpawn[0] + " is already occupied";
+
+    }
+    private void Update()
+    {
+        canvas.transform.LookAt(mainCam.transform);
+        if (defencePlacement.hasBeenPlaced)
+        {
+            NumberText.text = "";
+            NumberText.gameObject.SetActive(false);
+        }
+    }
+}
