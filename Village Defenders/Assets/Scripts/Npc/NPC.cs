3e4e1fa42dd5411d2e6e4885240bcb578b606403
Vasster <vasilhspinhs@gmail.com>
2024-02-02 02:18:15 +0200

    Change Npc behaviors
    -Changed Npc's behaviors from my own physics to using unity engine AI. Also added nav mesh obstacles so the npc can avoid stuff such as houses etc.

--- Village Defenders/Assets/Scripts/Npc/NPC.cs
+++ Village Defenders/Assets/Scripts/Npc/NPC.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPC : MonoBehaviour
 {
@@ -13,8 +14,7 @@ public class NPC : MonoBehaviour
     private Transform mainCamera;
     [SerializeField] private float npcRotateSpeed = 180;
     private LayerMask npcLayerMask;
-    [SerializeField] private LayerMask obstacleLayerMask;
-    [SerializeField] private float obstacleDetectionDistance = 2f;
+    private NavMeshAgent navMeshAgent;
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -26,44 +26,36 @@ public class NPC : MonoBehaviour
         PopulationManager.instace.UpdatePopulationUI();
         mainCamera = Camera.main.transform;
         npcLayerMask = 1 << LayerMask.NameToLayer("NPC");
-        obstacleLayerMask = 1 << LayerMask.NameToLayer("Obstacle");
+
         npcRotateSpeed = 180f;
-        obstacleDetectionDistance = 4f;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.stoppingDistance = 0.1f;
     }
 
     void Update()
     {
         MoveTowardsTarget();
         CanvasFaceCamera();
-        Debug.Log(obstacleDetectionDistance);
+
     }
 
     private void MoveTowardsTarget()
     {
-        if (transform.position != targetLocation)
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
         {
-            Vector3 direction = (targetLocation - transform.position).normalized;
-            var step = npcWalkSpeed * Time.deltaTime;
 
-            RaycastHit hit;
-
-           
-            if (Physics.Raycast(transform.position, direction, out hit, obstacleDetectionDistance, obstacleLayerMask))
-            {
-                
-                targetLocation = Vector3.Lerp(targetLocation, hit.point + hit.normal * obstacleDetectionDistance, 0.5f);
-                direction = (targetLocation - transform.position).normalized;
-             
-            }
-
-            transform.position = Vector3.MoveTowards(transform.position, targetLocation, step);
-            Quaternion targetRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, npcRotateSpeed * Time.deltaTime);
-        }
-        else
-        {
             targetLocation = RentacleZone.instance.GetRandomPoint();
+            navMeshAgent.SetDestination(targetLocation);
         }
+
+
+        Quaternion targetRotation = Quaternion.LookRotation(navMeshAgent.velocity.normalized);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, npcRotateSpeed * Time.deltaTime);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, npcRotateSpeed * Time.deltaTime);
+
+
+
     }
     private void CanvasFaceCamera()
     {
@@ -84,7 +76,7 @@ public class NPC : MonoBehaviour
     void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, obstacleDetectionDistance);
+     
     }
 }
 
