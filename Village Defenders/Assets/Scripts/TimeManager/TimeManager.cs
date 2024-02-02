9d1ea1d31d4710297dc30dc99c776d653d876235
Vasster <vasilhspinhs@gmail.com>
2024-02-02 05:32:59 +0200

    Fixed Sliders being interactable
    -Fixed Sliders being interactable
    -Fixed Speed time button when you clicked the button it sped the time and then you clicked anywhere it sped the time.
    -Added Normal time/Sped time text.

--- Village Defenders/Assets/Scripts/TimeManager/TimeManager.cs
+++ Village Defenders/Assets/Scripts/TimeManager/TimeManager.cs
@@ -1,42 +1,42 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
-    //speed time button 
     [SerializeField] private Button speedTimeButton;
-    //bool to check if our button is being clicked
     [SerializeField] private bool isButtonBeingClicked;
-   public void ClickedOnSpeedButton()
+    [SerializeField] private TextMeshProUGUI timeSpeedText;
+    private void Start()
     {
-        isButtonBeingClicked = true;
+        isButtonBeingClicked = false;
+        timeSpeedText.text = "Time Speed:Normal";
     }
-    private void Update()
+    public void ClickedOnSpeedButton()
     {
-        //if we click on it and the button can be clicked
-        if (Input.GetMouseButtonDown(0) && !isButtonBeingClicked)
+        isButtonBeingClicked = !isButtonBeingClicked;
+
+        if (isButtonBeingClicked)
         {
             SpeedUpTime();
         }
-        //if we release the mouse button on it and the button can be clicked
-    else    if (Input.GetMouseButtonUp(0) &&isButtonBeingClicked)
+        else
         {
-
             ResetTime();
         }
-
     }
+
     private void SpeedUpTime()
     {
-            isButtonBeingClicked = true;
-            Time.timeScale = 2.5f;
-            Debug.Log("Button is being pressed all the time");
-        }
-    
+        Time.timeScale = 2.5f;
+        timeSpeedText.text = "Time Speed:Double";
+        Debug.Log("Time is sped up");
+    }
+
     private void ResetTime()
     {
-        isButtonBeingClicked = false;
         Time.timeScale = 1.0f;
-        Debug.Log("Button is not being pressed");
+        timeSpeedText.text = "Time Speed:Normal";
+        Debug.Log("Time is reset");
     }
 }
